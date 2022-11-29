390ff152d8974c0bac8abc470b553588488c99b2
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-11-29 23:50:13 +0545

    awareness program

--- signup.py
+++ signup.py
@@ -3,12 +3,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+import os
+
 
 def proceed():
     xp_proceed = '//*[@id="q-app"]/div/div/div/main/div[1]/div/section/div/div/div[2]/div/div[2]/form[1]/div[2]/div[2]/button/span[2]/span'
     btn_proceed = browser.find_element(By.XPATH, xp_proceed)
     btn_proceed.click()
-    time.sleep(4)
+    print('click!!!!!!!!!!')
 
 
 browser = webdriver.Chrome()
@@ -24,20 +26,30 @@ xp_mobile = '/html/body/div[1]/div/div/div/main/div[1]/div/section/div/div/div[2
 input_mobile = browser.find_element(By.XPATH, xp_mobile)
 input_mobile.send_keys(9869278633)
 
-xp_proceed = '//*[@id="q-app"]/div/div/div/main/div[1]/div/section/div/div/div[2]/div/div[2]/form[1]/div[2]/div[2]/button/span[2]/span'
-btn_proceed = browser.find_element(By.XPATH, xp_proceed)
-btn_proceed.click()
+proceed()
 time.sleep(3)
 
-
+counter = 0
 success = False
 msg_quota = 'Sorry, Registration quota for today is full. Please try again next day.'
-while not success:
-    body = browser.find_element(By.TAG_NAME, 'body')
-    if msg_quota in body.text:
-        proceed()
-    else:
-        success = True
 
-print('Get the otp. HURRY!!!!!!!!!!!!!!!')
+while not success:
+    text_body = browser.find_element(By.TAG_NAME, 'body').text
+    print(f'{text_body[-149:]}')
+
+    if msg_quota not in text_body:
+        success = True
+    else:
+        print('sleep!!!!!!!!!!!!!!!!!!')
+        time.sleep(60)
+        print('repeat!!!!!!!!!!!!!!!!!!!!! \n')
+        print(counter)
+
+        counter += 1
+        proceed()
+        time.sleep(3)
+
+print(f'\n\n Got the otp on {counter} retry. HURRY!!!!!!!!!!!!!!!\n')
+
+os.system('alacritty -e python3 /home/hawk/beeper.py')
 time.sleep(99999)
